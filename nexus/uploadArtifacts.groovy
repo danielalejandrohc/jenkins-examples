edbb8d6d2991310777068a490ba66319359708dc
danielalejandrohc <daniel.alejandro.hc@everymundo.com>
2019-01-06 23:06:00 -0600

    use ant glob pattern and adjust the code

--- nexus/uploadArtifacts.groovy
+++ nexus/uploadArtifacts.groovy
@@ -1,73 +1,86 @@
 pipeline {
+
     agent {
         label "master"
     }
 
+    tools {
+        // Note: this should match with the tool name configured in your jenkins instance (JENKINS_URL/configureTools/)
+        maven "Maven 3.6.0"
+    }
+
     environment {
         NEXUS_VERSION = "nexus3"
-        NEXUS_PROTOCOL = "https"
-        NEXUS_URL = "localhost:3333/nexus"
+        NEXUS_PROTOCOL = "http"
+        NEXUS_URL = "localhost:8081"
         NEXUS_REPOSITORY = "repository-example"
-        NEXUS_CREDENTIAL_ID = "jenkins"
+        NEXUS_CREDENTIAL_ID = "nexus-credentials"
     }
 
-    stage("clone java code") {
-        steps {
-            script {
-
-            }
-        }
-    }
-
-    stage("mvn build") {
-        steps {
-            script {
-                sh "mvn package"
-            }
-        }
-    }
-
-    stage("publish to nexus") {
-        steps {
-            script {
-                // Read POM xml file using 'readMavenPom' step 
-                pom = readMavenPom file: POM_LOCATION_PARAM;
-                // Build the .jar file name, usually this is compose by: artifactId-version.packaging from the pom.xml file
-                jarFileName = "${pom.artifactId}-${pom.version}.${pom.packaging}";
-                // By default java maven artifacts will be located at: target/*.jar folder, (or .war, .ear in that's the case)
-                jarFilePath = "target/${jarFileName}";
-                // Assign to a boolean response verifying If the artifact name exists
-                jarFilePathExists = fileExists jarFilePath;
-                
-                if(jarFilePathExists) {
-                    echo "*** File: ${jarFilePath}, group ${pom.groupId} version type ${pom.packaging}";
-                    
-                    nexusArtifactUploader(
-                        nexusVersion: NEXUS_VERSION,
-                        protocol: NEXUS_PROTOCOL,
-                        nexusUrl: NEXUS_URL,
-                        groupId: pom.groupId,
-                        version: pom.version,
-                        repository: NEXUS_REPOSITORY,
-                        credentialsId: NEXUS_CREDENTIAL_ID,
-                        artifacts: [
-                            // Artifact generated such as .jar, .ear and .war files.
-                            [artifactId: pom.artifactId,
-                            classifier: '',
-                            file: jarFilePath,
-                            type: pom.packaging]
-
-                            // Lets upload the pom.xml file for additional information for Transitive dependencies
-                            [artifactId: pom.artifactId,
-                            classifier: '',
-                            file: "pom.xml",
-                            type: "pom"]
-                        ]
-                    );
-                } else {
-                    error "*** File: ${jarFilePath}, could not be found";
+    stages {
+        stage("clone java code") {
+            steps {
+                script {
+                    // Lets clone the source
+                    git 'https://github.com/javaee/cargotracker.git';
                 }
             }
         }
+
+        stage("mvn build") {
+            steps {
+                script {
+                    // If you are using Windows then you should use "bat" step
+                    // Since unit testing is out of the scope, we skip them
+                    sh "mvn package -DskipTests=true"
+                }
+            }
+        }
+
+        stage("publish to nexus") {
+            steps {
+                script {
+                    // Read POM xml file using 'readMavenPom' step 
+                    pom = readMavenPom file: "pom.xml";
+                    // Find built artifact
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    // By default java maven artifacts will be located at: target/*.jar folder, (or .war, .hpi, .ear in that's the case)
+                    artifactPath = filesByGlob[0].path;
+                    // Assign to a boolean response verifying If the artifact name exists
+                    artifactExists = fileExists artifactPath;
+                    
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        
+                        nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                // Artifact generated such as .jar, .ear and .war files.
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+
+                                // Lets upload the pom.xml file for additional information for Transitive dependencies
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                    }
+                }
+            }
+        }
+        
     }
 }
