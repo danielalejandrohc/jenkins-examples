87eb3a2e2f1dd88b18c5f9a5572cdd06f07d89e8
daniel.alejandro.hc@gmail.com <daniel.alejandro.hc@gmail.com>
2020-02-17 14:30:19 -0600

    Microservice code

--- openshift/microservice/src/main/java/com/danielalejandrohc/microservice/MicroserviceApplication.java
+++ openshift/microservice/src/main/java/com/danielalejandrohc/microservice/MicroserviceApplication.java
@@ -4,12 +4,14 @@ package com.danielalejandrohc.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
 	}
-
 
 }
